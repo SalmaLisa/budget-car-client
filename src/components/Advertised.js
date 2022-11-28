204a0f01493a418a25248b2fbec104707cfe7ac8
Salmalisa <salmalisa7342@gmail.com>
2022-11-28 19:07:15 +0600

    advertise section responsive done

--- src/components/Advertised.js
+++ src/components/Advertised.js
@@ -20,41 +20,70 @@ const Advertised = () => {
   console.log(advertisedProducts);
   return (
     <>
-    {
-      advertisedProducts.length !== 0 &&
-      <>
-      <div className="bg-yellow-300 w-2/3 mx-auto h-[20px]"></div>
-      <div className="bg-base-300 p-16 pb-40">
-        <h1 className="text-4xl text-center mb-16 italic">
-          {" "}
-          <span className="text-yellow-500 text-6xl">Latest</span> Collections
-        </h1>
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          slidesPerGroup={3}
-          loop={true}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
-          }}
-          loopFillGroupWithBlank={true}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper"
-        >
-          {advertisedProducts.map((advertisedProduct) => (
-            <SwiperSlide key={advertisedProduct._id}>
-              <SlideCard advertisedProduct={advertisedProduct}></SlideCard>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      {advertisedProducts.length !== 0 && (
+        <>
+          <div className="bg-yellow-300 w-2/3 mx-auto h-[16px] lg:h-[20px]"></div>
+          <div className="bg-base-300 lg:p-16 pt-5 pb-36 lg:pb-40">
+            <h1 className="text-4xl text-center lg:mb-16 mb-5 italic">
+              {" "}
+              <span className="text-yellow-500 text-6xl">Latest</span>{" "}
+              Collections
+            </h1>
+            <div className="hidden md:block">
+              <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                slidesPerGroup={3}
+                loop={true}
+                autoplay={{
+                  delay: 5000,
+                  disableOnInteraction: false,
+                }}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+              >
+                {advertisedProducts.map((advertisedProduct) => (
+                  <SwiperSlide key={advertisedProduct._id}>
+                    <SlideCard
+                      advertisedProduct={advertisedProduct}
+                    ></SlideCard>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+            <div className="md:hidden">
+              <Swiper
+                loop={true}
+                autoplay={{
+                  delay: 5000,
+                  disableOnInteraction: false,
+                }}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+              >
+                {advertisedProducts.map((advertisedProduct) => (
+                  <SwiperSlide key={advertisedProduct._id}>
+                    <SlideCard
+                      advertisedProduct={advertisedProduct}
+                    ></SlideCard>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+        </>
+      )}
     </>
-    }</>
   );
 };
 
